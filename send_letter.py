cb81aca642214fc5864cdd794c51579bf23d3eb2
Koterev Maksim <makoterev@gmail.com>
2021-02-02 20:54:04 +0300

    Separated code in functions

--- send_letter.py
+++ send_letter.py
@@ -1,3 +1,4 @@
+#!/home/mkoterev/Python/newmon/monitoring/venv/bin/python3
 from datetime import datetime
 from pathlib import Path
 import json
@@ -8,46 +9,69 @@ from email.header import Header
 from email import encoders
 import smtplib
 
-addr_to   = ["zedix.ru@gmail.com", "mr.lux1@yandex.ru"]
 addr_from = "makoterev@gmail.com"
 
 # Хранение пароля от почты в открытом доступе - плохая идея
 # По этому пароль будет храниться в локальном файле на компьютере
-with open("private_data.json") as json_file:
+
+working_directory = Path(__file__).parent
+
+with open(working_directory / "private_data.json") as json_file:
     private_info = json.load(json_file)
 
 password = private_info["email_pd"]
-print(password)
 
-today_date = datetime.today().strftime("%d-%m-%y-")
-path_to_xl_file = Path(f"reports/{today_date}report.xlsx")
-filename_xl = f"{today_date}report.xlsx"
-path_to_csv_file = Path(f"data/{today_date}info.csv")
-filename_csv = f"{today_date}info.csv"
 
-msg = MIMEMultipart()
-msg['From']    = addr_from
-msg['To']      = ', '.join(addr_to)
-msg['Subject'] = Header(f'Отчет практика {today_date[:-1]}', 'utf-8')
+def send_letter(msg):
+    server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+    server.set_debuglevel(False)
+    server.login(addr_from, password)
+    server.send_message(msg)
+    server.quit()
 
-body = f"Отчет за {today_date[:-1]} число\nОтчет сформирован автоматически, пожалуйста, не отвечайте на него."
-msg.attach(MIMEText(body, 'plain', 'utf-8'))
 
-part = MIMEBase('application', "octet-stream")
-part.set_payload(open(path_to_xl_file, "rb").read(), 'utf-8')
-encoders.encode_base64(part)
-part.add_header('Content-Disposition', f'attachment; filename="{filename_xl}"')
-msg.attach(part)
+def form_message(to, subject, text):
+    msg = MIMEMultipart()
+    msg['From'] = addr_from
+    msg['To'] = ', '.join(to)
+    msg['Subject'] = Header(subject, 'utf-8')
 
-part = MIMEBase('application', "octet-stream")
-part.set_payload(open(path_to_csv_file, "rb").read(), 'utf-8')
-encoders.encode_base64(part)
-part.add_header('Content-Disposition', f'attachment; filename="{filename_csv}"')
-msg.attach(part)
+    body = text
+    msg.attach(MIMEText(body, 'plain', 'utf-8'))
+    return msg
 
-server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-server.set_debuglevel(False)  
-server.login(addr_from, password)
-server.send_message(msg)
-server.quit()
-# UyP2EOtjuo7
+
+def main():
+    addr_to = ["zedix.ru@gmail.com", "mr.lux1@yandex.ru"]
+
+    today_date = datetime.today().strftime("%d-%m-%y-")
+    path_to_xl_file = working_directory / f"reports/{today_date}report.docx"
+    filename_xl = f"{today_date}report.docx"
+    path_to_csv_file = working_directory / f"data/{today_date}info.csv"
+    filename_csv = f"{today_date}info.csv"
+
+    msg = form_message(
+        addr_to,
+        f'Отчет практика {today_date[:-1]}',
+        f"Отчет за {today_date[:-1]} число\nОтчет сформирован автоматически, пожалуйста, не отвечайте на него."
+    )
+
+    part = MIMEBase('application', "octet-stream")
+    part.set_payload(open(path_to_xl_file, "rb").read(), 'utf-8')
+    encoders.encode_base64(part)
+    part.add_header('Content-Disposition',
+                    f'attachment; filename="{filename_xl}"')
+    msg.attach(part)
+
+    part = MIMEBase('application', "octet-stream")
+    part.set_payload(open(path_to_csv_file, "rb").read(), 'utf-8')
+    encoders.encode_base64(part)
+    part.add_header('Content-Disposition',
+                    f'attachment; filename="{filename_csv}"')
+    msg.attach(part)
+
+    send_letter(msg)
+
+
+if __name__ == '__main__':
+    main()
