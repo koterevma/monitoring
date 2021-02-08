e41e3b25284863e00e9840e009cee6fb66ee2b54
JLoGukA <JLoGuka@gmail.com>
2021-02-08 16:00:36 +0300

    Update alt_report.py

--- alt_report.py
+++ alt_report.py
@@ -1,19 +1,35 @@
+from numpy.core.fromnumeric import size
 import pandas as pd
 import datetime
 import docx.enum.text
+import urllib.request
+import odf
 from pathlib import Path
 from docx import Document
 from docx.shared import RGBColor
-from docx.enum.text import WD_ALIGN_PARAGRAPH
+from docx.shared import Pt
+from docx.enum.text import WD_ALIGN_PARAGRAPH, WD_BREAK
 
 from docx.oxml.ns import nsdecls
 from docx.oxml import parse_xml
 
 
 def main():
-    today_date = datetime.date.today().strftime("%d-%m-%y-")
-    path_file_in = Path(f"csv.csv")
-    path_file_out = Path(f"data.docx")
+
+    #INITIALIZATION PART 0
+
+    brigades = ['Цыбулин П. А.\nМарков Г. С.\n', 'Базылев А. А.\nЯшин Н. А.\n', 'Патокин С. В.\nЗаидов А. Р.\n', 'Савченко В. А.\nЗайцев А. И.\n',
+                    'Ольховик Н. С.\nЩербаков В. Ю.\n','Носков М. П.\nЦветков Ю. А.\n', 'Чупис С. А.\nФролова П. В.\n', 'Носков И. В.\nЛегин Д. И.\n',
+                    'Красовский Г. О.\nШнырина А. Н.\n', 'Тебеньков К. А.\nМашкарь М. Ю.\n', 'Воскресенский О. А.\nАртёмов А. А.\n', 'Котерев М. А.\nМихальченко М. Е.\n'
+                    'Кетрой Д. А.\nЛинкевич М. М.\n', 'Соловьев П. А.\nЗавьялов И. А.\n', 'Лямин А. С.\nСанжурин С. В.\n', 'Фалин А. Д.\nПолторацкий С. А.\n']
+
+    today_date = datetime.date.today().strftime("%d-%m-%y")
+    path_file_in = Path(f"py\csv.csv")
+    path_file_out = Path(f"py\data.docx")
+    url = 'https://drive.google.com/u/0/uc?id=1dgdUBBmExKNNZmom_JVUfgHPXcwTN9bk&export=download'
+    currbrigade = list()
+
+    #TABLE READ PART 1
 
     with open(path_file_in, 'r', encoding="utf-8") as file:
         df = pd.read_csv(file, delimiter=';')
@@ -37,6 +53,64 @@ def main():
     shade_row = list()
     shade_col = list()
 
+    #FILE DOWNLOAD AND BRIGADE READ PART 2
+
+    urllib.request.urlretrieve(url, 'py\sch.ods')
+    schtab = pd.read_excel("py\sch.ods", engine="odf")
+
+    clm = 0
+    current_date = str(dates[0])[0:10]+' 00:00:00'
+    for i in range(schtab.shape[1]):
+         if(str(schtab.iloc[1,i])==current_date):
+             clm = i
+             print('FOUND')
+             break
+    thisday = list(schtab.iloc[:,clm])
+    for i in range(len(thisday)):
+        if (thisday[i]==1)|(thisday[i]==2)|(thisday[i]==5):
+            currbrigade.append((i-1,brigades[i-2]))
+
+    #TITLE CREATION PART 3
+
+    p1 = doc.add_paragraph()
+    p1.alignment = WD_ALIGN_PARAGRAPH.CENTER
+
+    p2 = doc.add_paragraph()
+    p2.alignment = WD_ALIGN_PARAGRAPH.RIGHT
+
+    p3 = doc.add_paragraph()
+    p3.alignment = WD_ALIGN_PARAGRAPH.CENTER
+
+    p1r1 = p1.add_run('Министерство науки и высшего образования\n Российской Федерации Мытищинский филиал\n')
+    p1r1.add_picture('py\h.png')
+    p1r1.add_text('\nФедеральное государственное бюджетное образовательное\n учреждение\n высшего образования\n' 
+                   + '«Московский государственный технический университет\n имени Н.Э. Баумана\n' 
+                   + '(национальный исследовательский университет)»\n(МГТУ им. Н.Э. Баумана)\n')
+    p1r1.add_text('______________________________________________________________________________________________\n'
+                  + '______________________________________________________________________________________________\n')
+    p1r1.font.bold=True
+    p1r1.font.size = Pt(12)
+    
+    p1r2 = p1.add_run('ФАКУЛЬТЕТ Космический\n КАФЕДРА «Прикладная математика, информатика и вычислительная техника» K3-МФ\n\n\n')
+    p1r2.font.size = Pt(14)
+    p1r2.font.bold = True
+
+    p1r3 = p1.add_run('Отчет\n По дежурству\n\n')
+    p1r3.font.size = Pt(28)
+    p1r3.font.bold = True
+
+    p2r1 = p2.add_run('Бригада: '+ str(currbrigade[0][0])+'\n'+currbrigade[0][1]+'\n')
+    p2r1.add_text('Бригада: '+str(currbrigade[1][0])+'\n'+currbrigade[1][1]+'\n')
+    p2r1.add_text('Бригада: '+str(currbrigade[2][0])+ '\n'+currbrigade[2][1]+'\n\n')
+    p2r1.font.size = Pt(14)
+
+    p3r1 = p3.add_run('Дата прохождения дежурства: '+current_date[0:10]+'\n\n\n'+'Москва '+current_date[0:4]+'\n\n')
+    p3r1.font.size = Pt(14)
+    p3r1.font.bold = True
+    p3r1.add_break(WD_BREAK.PAGE)
+
+    #TABLE CREATION PART 4
+
     tablecount = (len(dates)-1)/5
     if tablecount%1!=0: tablecount = int(tablecount)+1
 
@@ -59,7 +133,9 @@ def main():
             tables[i].cell(j+1,0)._tc.get_or_add_tcPr().append(shade_col[m])
             paragraph = tables[i].cell(j + 1, 0).paragraphs[0]
             paragraph.alignment = WD_ALIGN_PARAGRAPH.CENTER
-            paragraph.add_run(df.columns[j]).font.bold = True
+            trun = paragraph.add_run(df.columns[j])
+            trun.font.bold = True
+            trun.font.size = Pt(8)
             m=m+1
 
         for j in range(columns-1):
@@ -68,7 +144,9 @@ def main():
             tables[i].cell(0,j+1)._tc.get_or_add_tcPr().append(shade_row[k])
             paragraph = tables[i].cell(0, j + 1).paragraphs[0]
             paragraph.alignment = WD_ALIGN_PARAGRAPH.CENTER
-            paragraph.add_run(dates[k].strftime("%H:%M:%S - ") + dates[k+1].strftime("%H:%M:%S")).font.bold = True
+            trun = paragraph.add_run(dates[k].strftime("%H:%M:%S - ") + dates[k+1].strftime("%H:%M:%S"))
+            trun.font.bold = True
+            trun.font.size = Pt(8)
             k=k+1
     
     k = 0
