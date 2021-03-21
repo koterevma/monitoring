7b8a7e69441c044ae3a411c4a83a3623745e23b2
Koterev Maksim <makoterev@gmail.com>
2021-03-21 22:30:05 +0300

    Now sending messages should work

--- main.py
+++ main.py
@@ -41,6 +41,7 @@ devices = [
     "01 РОСА%20К-2",
     "01 Роса-К-1",
     "schHome Тест%20Студии",
+    "01 Опорный%20барометр",
     "schHome Тест%20воздуха",
     "01 Hydra-L",
     "02 Hydra-L",
@@ -73,21 +74,12 @@ def send_error_message(errors):
                 pass
     print("Sending error message with email")
     msg = send_letter.form_message(
-        ["zedix.ru@gmail.com", "sch-ru@yandex.ru"],
+        ["zedix.ru@gmail.com"], #, "sch-ru@yandex.ru"],
         f"Ошибки {today_date[:-1]}",
-        "Только что, {}, в ходе работы скриптов, следящих за состоянием сайтов и приборов вуза были получены следующие ошибки:\n{}".format(datetime.datetime.today().strftime('%d.%m.%y %H:%M'), '\n'.join(errors))
+        "Только что, {}, в ходе работы скриптов, следящих за состоянием сайтов и приборов вуза были получены следующие ошибки:\n{}\n\nСообщение было создано и отправлено автоматически".format(datetime.datetime.today().strftime('%d.%m.%y %H:%M'), '\n'.join(errors))
     )
     print(msg)
     send_letter.send_letter(msg)
-
-
-# Для терминала bash, можно будет потом избавиться от цветовых оформлений
-class bcolors:
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
 
 
 def form_names(file):
@@ -100,21 +92,28 @@ def form_names(file):
     # print(';'.join(names))
 
 
+def handle_site_err(err_text, statuses, name, err):
+    statuses.append('0')
+    print(f"Error with connection to {name} ({err})")
+    error_text.append(
+        f"Ошибка подключения к сайту {name} ({err})")
+    
+
+def handle_dev_error(error_text, dev_dict, err):
+    print(f"Error with connection to db_api_REST ({req.status_code})")
+    error_text.append(
+        f"Ошибка подключения к REST api ({req.status_code})")
+    
+
 if __name__ == "__main__":
-    test_req = requests.get('http://google.com')
-    if test_req.status_code != 200:
-        print(
-            f"{bcolors.FAIL}Error. No internet connection ({test_req.status_code}){bcolors.ENDC}")
+    try:
+        test_req = requests.get('http://google.com')
+    except requests.exceptions.ConnectionError as e:
+        print("Error. No internet connection (" + str(e) + ")")
     else:
         error_text = []
         # print(today_date)
         path_to_data = working_directory / "data"
-
-        # Mayby it's a good idea to move some init setup such as creating
-        # folders for files into another script like setup.py.
-        # Virtual enviroment could be also set up from such script
-        if not path_to_data.exists():
-            os.mkdir(path_to_data)
 
         path_file_out = path_to_data / f"{today_date}info.csv"
         if not path_file_out.is_file():
@@ -127,28 +126,27 @@ if __name__ == "__main__":
         statuses = []
 
         for name, url in urls.items():
-            req = requests.get(url)
-            if req.status_code == 201:
+            try:
+                req = requests.get(url)
+            except requests.exceptions.ConnectionError as e:
+                pr_bar.clearln()
+                handle_site_err(error_text, statuses, name, str(e))
+            if req.status_code == 200:
                 statuses.append('1')
             else:
-                statuses.append('0')
                 pr_bar.clearln()
-                print(
-                    f"{bcolors.FAIL}Error with connection to {name}. Code: {req.status_code}{bcolors.ENDC}")
-                error_text.append(
-                    f"Ошибка подключения к сайту {name}. Код: {req.status_code}")
+                handle_site_err(error_text, statuses, name, str(req.status_code))
             pr_bar.next()
 
         dev_dict = dict([(i, '0') for i in devices])
         url = 'http://webrobo.mgul.ac.ru:3000/db_api_REST/not_calibr/last5min/'
-        req = requests.get(url)
-
+        try:
+            req = requests.get(url)
+        except requests.exceptions.ConnectionError as e:
+            handle_dev_error(error_text, dev_dict, str(e))
         if req.status_code != 200:
             pr_bar.clearln()
-            print(
-                f"{bcolors.FAIL}Error with connection to db_api_REST. Code: {req.status_code}{bcolors.ENDC}")
-            error_text.append(
-                f"Ошибка подключения к REST api. Код: {req.status_code}")
+            handle_dev_error(error_text, dev_dict, str(req.status_code))
         else:
             data_in_json = json.loads(req.text)
             for record in data_in_json:
@@ -165,7 +163,7 @@ if __name__ == "__main__":
             if status == '0':
                 pr_bar.clearln()
                 print(
-                    f"{bcolors.WARNING}Warning. No data for {uname.replace('%20', ' ')} {serial} has found{bcolors.ENDC}")
+                    f"Warning. No data for {uname.replace('%20', ' ')} {serial} has found")
         statuses.extend(dev_dict.values())
 
         with open(path_file_out, 'a', encoding='utf-8') as file:
